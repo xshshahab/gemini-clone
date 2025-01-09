44b914979e6149b491df9b437491849ba3976db5
xshshahab <mdshahbuddin9504@gmail.com>
2025-01-09 13:10:58 +0530

    gemini api integrated

---
+++ src/config/gemini.js
@@ -0,0 +1,32 @@
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const apiKey = "AIzaSyDbmG8uNTir-HxPxuHzjonXMcgzc-5gUg8";
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash-exp",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  console.log(result.response.text());
+}
+
+export default run;
---
+++ src/context/Context.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+import run from "../config/gemini";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+
+    const [input, setInput] = useState("");
+    const [recentPrompt, setRecentPrompt] = useState("");
+    const [previousPrompt, setPreviousPrompt] = useState([]);
+    const [showResult, setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [resultData, setResultData] = useState("");
+
+    const onSent = async (prompt) => {
+        await run(prompt)
+    }
+
+
+    const contextValue = {}
+
+    return (
+        <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+    )
+}
+
+export default ContextProvider
